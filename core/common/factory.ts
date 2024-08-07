5854ab0da224de6ac7d099f6190da7571dbfa0e3
jhondesignee <jhondesignee@gmail.com>
2024-08-07 20:19:48 -0300

    feat: add loadable interface
    Game Objects and Logics now extends of Loadable
    Game Objects cannot listen to events anymore
    Tilemap is no longer needed. A Logic can be created for that
    Factory types changed

--- core/common/factory.ts
+++ core/common/factory.ts
@@ -1,17 +1,23 @@
 type Constructable = new (...args: Array<any>) => any
+type Args<T extends Constructable> = ConstructorParameters<T>
 
-export default class Factory<Class extends Constructable> {
+export interface FactoryInterface<T> {
+  getInstance(ID: string, args?: Array<any>): T | never
+  getInstances(): Array<T>
+}
+
+export default class Factory<Class extends Constructable> implements FactoryInterface<Class> {
   private readonly _class: Class
-  private readonly defaultArgs: ConstructorParameters<Class>
+  private readonly defaultArgs: Args<Class>
   private readonly instances: Map<string, Class>
 
-  constructor(_class: Class, defaultArgs: ConstructorParameters<Class>) {
+  constructor(_class: Class, defaultArgs: Args<Class>) {
     this._class = _class
     this.defaultArgs = defaultArgs
     this.instances = new Map()
   }
 
-  public getInstance(ID: string, args?: ConstructorParameters<Class>): Class | never {
+  public getInstance(ID: string, args?: Args<Class>): Class | never {
     if (this.instances.has(ID)) return this.instances.get(ID)!
     const instance = new this._class(...[...this.defaultArgs, ...(args ? args : [])])
     this.instances.set(ID, instance)
